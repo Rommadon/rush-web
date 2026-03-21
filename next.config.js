f16b17742977c42d708dbc5073ecd90f07b79d80
= <=>
2026-03-21 16:30:27 +0700

    feat: disable Next.js image optimization and add mise configuration for Yarn 1.

--- next.config.js
+++ next.config.js
@@ -29,6 +29,7 @@ const moduleExports = {
     disableClientWebpackPlugin: true,
   },
   images: {
+    unoptimized: true,
     domains: [
       'staging-shopdit.s3.ap-southeast-1.amazonaws.com',
       'staging-shopdit.s3.amazonaws.com',
